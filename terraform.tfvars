bad06f4116ac44eac5dd0511d004bcb1caa58e93
Alireza <allirreza@users.noreply.github.com>
2020-11-10 00:17:03 +0100

    Created environment variable
    Created environment variable to use in Cluster name and tags

--- terraform.tfvars
+++ terraform.tfvars
@@ -2,3 +2,4 @@
   aws_access_key = "Your-AWS-Access-Key"
   aws_secret_key = "Your-AWS-Secret-Key"
   domain = "your-domain-address"
+  environment_name = "development"
--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "development-eks-${random_string.suffix.result}"
+  cluster_name = "${var.environment_name}-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -20,7 +20,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.6.0"
 
-  name                 = "development-vpc"
+  name                 = "${var.environment_name}-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
